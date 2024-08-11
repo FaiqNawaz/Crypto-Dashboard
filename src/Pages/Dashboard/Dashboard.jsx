4668b6620c22f7b8a0a456b0b7f7ec94bf83c964
FaiqNawaz <faiqnawaz92@gmail.com>
2024-08-11 17:02:18 +0500

    Top portfolio added

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -3,11 +3,12 @@ import Sidenav from "../../Components/Sidenav";
 import TopNav from "../../Components/TopNav";
 import { Flex, Box } from "@chakra-ui/react";
 import DashboardLayout from "../../Components/DashboardLayout";
+import PortfolioSection from "./PortfolioSection";
 
 const Dashboard = ({ title }) => {
   return (
     <DashboardLayout title="Dashboard">
-      <div>How Are You?</div>
+      <PortfolioSection />
     </DashboardLayout>
   );
 };
--- src/Pages/Dashboard/PortfolioSection.jsx
+++ src/Pages/Dashboard/PortfolioSection.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { HStack, Stack, Icon, Text, Tag, Button } from "@chakra-ui/react";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 
 const PortfolioSection = () => {
-  return <div></div>;
+  return (
+    <HStack bg="white" borderRadius="xl" p="6" justify="space-between">
+      <HStack spacing="16">
+        <Stack>
+          <HStack color="black.80">
+            <Text fontSize="sm">Total Portfolio Value</Text>
+            <Icon as={AiOutlineInfoCircle} />
+          </HStack>
+          <Text textStyle="h2">Rs 112,312.24</Text>
+        </Stack>
+
+        <Stack>
+          <HStack color="black.80">
+            <Text fontSize="sm">Wallet Balance</Text>
+          </HStack>
+          <HStack>
+            <Text textStyle="h2">22.39401000</Text>{" "}
+            <Tag colorScheme="gray">BTC</Tag>
+            <Text textStyle="h2">Rs 1,300.00</Text>{" "}
+            <Tag colorScheme="gray">PKR</Tag>
+          </HStack>
+        </Stack>
+      </HStack>
+      <HStack>
+        <Button leftIcon={<Icon as={AiOutlineArrowDown} />}>Deposit</Button>
+        <Button leftIcon={<Icon as={AiOutlineArrowUp} />}>Withdraw</Button>
+      </HStack>
+    </HStack>
+  );
 };
 
 export default PortfolioSection;
