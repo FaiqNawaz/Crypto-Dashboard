8c070eed7bab2a627c084ddd93aea14e6e92a70e
FaiqNawaz <faiqnawaz92@gmail.com>
2024-08-12 21:59:23 +0500

    Top portfolio responsive

--- src/Pages/Dashboard/PortfolioSection.jsx
+++ src/Pages/Dashboard/PortfolioSection.jsx
@@ -5,24 +5,45 @@ import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 
 const PortfolioSection = () => {
   return (
-    <HStack bg="white" borderRadius="xl" p="6" justify="space-between">
-      <HStack spacing="16">
+    <HStack
+      bg="white"
+      borderRadius="xl"
+      p="6"
+      justify="space-between"
+      flexDir={{ base: "column", xl: "row" }}
+      align={{ base: "flex-start", xl: "center" }}
+    >
+      <HStack
+        spacing={{ base: "0", xl: "16" }}
+        flexDir={{ base: "column", xl: "row" }}
+        align={{ base: "flex-start", xl: "center" }}
+      >
         <Stack>
           <HStack color="black.80">
             <Text fontSize="sm">Total Portfolio Value</Text>
             <Icon as={AiOutlineInfoCircle} />
           </HStack>
-          <Text textStyle="h2">Rs 112,312.24</Text>
+          <Text textStyle="h2" fontWeight="medium">
+            Rs 112,312.24
+          </Text>
         </Stack>
 
         <Stack>
           <HStack color="black.80">
             <Text fontSize="sm">Wallet Balance</Text>
           </HStack>
-          <HStack>
-            <Text textStyle="h2">22.39401000</Text>{" "}
+          <HStack
+            spacing={4}
+            flexDir={{ base: "column", sm: "row" }}
+            align={{ base: "flex-start", sm: "center" }}
+          >
+            <Text textStyle="h2" fontWeight="medium">
+              22.39401000
+            </Text>{" "}
             <Tag colorScheme="gray">BTC</Tag>
-            <Text textStyle="h2">Rs 1,300.00</Text>{" "}
+            <Text textStyle="h2" fontWeight="medium">
+              Rs 1,300.00
+            </Text>{" "}
             <Tag colorScheme="gray">PKR</Tag>
           </HStack>
         </Stack>
