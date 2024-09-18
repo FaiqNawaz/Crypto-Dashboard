80d94ea2c113678e2a35ec20b7fae4dbd20d01be
FaiqNawaz <faiqnawaz92@gmail.com>
2024-09-18 16:19:26 +0500

    Transaction Page Setup

--- src/Pages/Dashboard/Transaction/TransactionPage.jsx
+++ src/Pages/Dashboard/Transaction/TransactionPage.jsx
@@ -1,18 +1,92 @@
 import React from "react";
 import DashboardLayout from "../../../Components/DashboardLayout";
-import { Button, Card, Flex, Icon } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  Flex,
+  HStack,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Tag,
+} from "@chakra-ui/react";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import TransactionTable from "./components/TransactionTable";
+import { IoSearchSharp } from "react-icons/io5";
+
+const tabs = [
+  {
+    name: "All",
+    count: 349,
+  },
+  {
+    name: "Deposit",
+    count: 114,
+  },
+  {
+    name: "Withdraw",
+    count: 55,
+  },
+  {
+    name: "Trade",
+    count: 50,
+  },
+];
 
 const TransactionPage = () => {
   return (
-    <DashboardLayout>
+    <DashboardLayout title="Transactions">
       <Flex justify="end" mt="6" mb="3">
         <Button leftIcon={<Icon as={MdOutlineFileDownload} />}>
           Export CSV
         </Button>
       </Flex>
 
-      <Card>Transaction Page</Card>
+      <Card>
+        <Tabs>
+          <TabList
+            pt="4"
+            w="full"
+            display="flex"
+            justifyContent="space-between"
+          >
+            <HStack>
+              {tabs.map((tab) => (
+                <Tab key={tab.name} gap="2" display="flex" pb="4">
+                  {tab.name}{" "}
+                  <Tag borderRadius="full" colorScheme="gray">
+                    {tab.count}
+                  </Tag>
+                </Tab>
+              ))}
+            </HStack>
+
+            <InputGroup maxWidth="200px" mr="6">
+              <InputLeftElement pointerEvents="none">
+                <Icon as={IoSearchSharp} />
+              </InputLeftElement>
+              <Input type="tel" placeholder="Search By ID" />
+            </InputGroup>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Card>
     </DashboardLayout>
   );
 };
